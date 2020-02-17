7293d540b8c6672b09663c2dd0fec7be839de325
Iskender,Borga A.B <b.iskender@student.fontys.nl>
2020-02-17 18:59:02 +0100

    v0.2 deleted all user controls and added panels

---
+++ Media Bazaar/MainAdmin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Media_Bazaar
+{
+    public partial class MainAdmin : Form
+    {
+        Login loginForm;
+        public MainAdmin()
+        {
+            InitializeComponent();
+            loginForm = DataManage.loginForm;
+            btnWhite.Visible = false;
+            pnlProfile.Visible = false;
+            pnlEmployees.Visible = false;
+            pnlSchedule.Visible = false;
+        }
+        private void makeVisibleOnly(Panel panel)
+        {
+            btnWhite.Visible = true;
+            pnlProfile.Visible = false;
+            pnlEmployees.Visible = false;
+            pnlSchedule.Visible = false;
+            panel.Visible = true;
+        }
+
+        private void makeHeightEqual(Button button) //To display the tab we are using
+        {
+            btnWhite.Height = button.Height;
+            btnWhite.Top = button.Top;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            loginForm.Show();
+            btnWhite.Height = btnLogout.Height;
+            btnWhite.Top = btnLogout.Top;
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            makeVisibleOnly(pnlProfile);
+            makeHeightEqual(btnProfile);
+        }
+
+        private void btnEmployees_Click(object sender, EventArgs e)
+        {
+            makeVisibleOnly(pnlEmployees);
+            makeHeightEqual(btnEmployees);
+        }
+
+        private void btnSchedule_Click(object sender, EventArgs e)
+        {
+            makeVisibleOnly(pnlSchedule);
+            makeHeightEqual(btnSchedule);
+        }
+    }
+}
