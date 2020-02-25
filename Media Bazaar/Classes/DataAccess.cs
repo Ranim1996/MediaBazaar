df6e492ef7cd5a95252594543776a58e3553b5ec
Yankov,Nikola N <n.yankov@student.fontys.nl>
2020-02-25 15:34:31 +0100

    ds

--- Media Bazaar/Classes/DataAccess.cs
+++ Media Bazaar/Classes/DataAccess.cs
@@ -13,20 +13,20 @@ namespace Media_Bazaar.Classes
     {
         public List<DBEmployee> GetDBEmployeesByLastName(string lastName)
         {
-            using (MySqlConnection connection = new MySqlConnection(Helper.CnnVal("DB")))                
+            using (MySqlConnection connection = new MySqlConnection(Helper.CnnVal("DB")))
             {
-                var output= connection.Query<DBEmployee>($"SELECT * FROM Employee WHERE LastName='{lastName}'").ToList();
+                var output = connection.Query<DBEmployee>($"SELECT * FROM Employee WHERE LastName='{lastName}'").ToList();
                 return output;
             }
-        }  
-        
-        public void InsertEmployee(string fName,string lName,string dateOfBirth,string email,string phoneNr,string nationality,string pos,string username,string password)
+        }
+
+        public void InsertEmployee(string fName, string lName, string dateOfBirth, string email, string phoneNr, string nationality, string pos, string username, string password)
         {
             using (MySqlConnection connection = new MySqlConnection(Helper.CnnVal("DB")))
             {
                 List<DBEmployee> employees = new List<DBEmployee>();
-               // employees.Add(new DBEmployee { FirstName = fName, LastName = lName, DateOfBirth = dateOfBirth, Email = email, PhoneNumber = phoneNr, Nationality = nationality, Position = pos, Username = username, Password = password });
-                connection.Execute(@"INSERT INTO Employee(FirstName,LastName,DateOfBirth,Email,PhoneNumber,Nationality,Position,Username,Password) VALUES (@fName,@lName,@dateOfBirth,@email,@phoneNr,@nationality,@pos,@username,password)",
+                // employees.Add(new DBEmployee { FirstName = fName, LastName = lName, DateOfBirth = dateOfBirth, Email = email, PhoneNumber = phoneNr, Nationality = nationality, Position = pos, Username = username, Password = password });
+                connection.Execute(@"INSERT INTO Employee(FirstName,LastName,DateOfBirth,Email,PhoneNumber,Nationality,Position,Username,Password) VALUES (@fName,@lName,@dateOfBirth,@email,@phoneNr,@nationality,@pos,@username,@password)",
                  new { FirstName = fName, LastName = lName, DateOfBirth = dateOfBirth, Email = email, PhoneNumber = phoneNr, Nationality = nationality, Position = pos, Username = username, Password = password });
             }
         }
