89537173abb213d81e3dc015071a57f38fd09419
Razvan <r.simion@student.fontys.nl>
2020-06-17 14:29:38 +0200

    first login changing password

--- MediaBazaarWeb/loginPage.php
+++ MediaBazaarWeb/loginPage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <link rel="stylesheet" type="text/css" href="static/css/main.css">
   <link rel="stylesheet" type="text/css" href="static/css/normalize.css">
@@ -11,67 +12,72 @@
   <meta name="HandheldFriendly" content="true">
   <title>Media Bazaar</title>
 </head>
+
 <body class="body-login-page">
 
   <div class="container">
-    
+
     <div class="flex-container">
       <div class="company-logo">
 
       </div>
     </div>
 
-      <form action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
-        <ul class="list">
-          <li><h2 id="login-heading">Employee login</h2></li>
-          <li><input type="text" name="username" placeholder="Username" id="username" required> </li>
-          <li><input type="password" name="password" placeholder="Password" id="password" required></li>
-          <?php 
-          require('./static/php/dbConnection.php');
-//header('location: homePage.php');
-//include('mediaBazaar/homePage.php');
-session_start();
+    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
+      <ul class="list">
+        <li>
+          <h2 id="login-heading">Employee login</h2>
+        </li>
+        <li><input type="text" name="username" placeholder="Username" id="username" required> </li>
+        <li><input type="password" name="password" placeholder="Password" id="password" required></li>
+        <?php
+        require('./static/php/dbConnection.php');
 
-if(isset($_POST['submit']))
-{
-    if(empty($_POST["username"]) || empty($_POST["password"]))
-    {
-        $message = '<label>All fields are required</label>';
-        echo $message;
-    }
-    else
-    {
-        $query = "SELECT * FROM employee WHERE username = :username AND password = :password AND ReasonsForRelease is NULL";
-        
-        $statement = $conn->prepare($query);
-        $statement->execute(
-            array(
+        session_start();
+
+        if (isset($_POST['submit'])) {
+          if (empty($_POST["username"]) || empty($_POST["password"])) {
+            $message = '<label>All fields are required</label>';
+            echo $message;
+          } else {
+            $query = "SELECT * FROM employee WHERE username = :username AND password = :password AND ReasonsForRelease is NULL";
+
+            $statement = $conn->prepare($query);
+            $statement->execute(
+              array(
                 'username' => $_POST["username"],
                 'password' => $_POST["password"]
-            )
-        );
-        $count = $statement->rowCount();
-        if($count > 0)
-        {
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['password'] = $_POST['password'];
-            $_SESSION['username'] = $_POST["username"];
-          header('Location: homePage.php');
-            exit;
-            //echo "Profile page";
+              )
+            );
+            $employees = $statement->fetchAll();
+            $count = $statement->rowCount();
+            if ($count > 0) {
+              $_SESSION['loggedin'] = TRUE;
+              $_SESSION['password'] = $_POST['password'];
+              $_SESSION['username'] = $_POST["username"];
+              foreach ($employees as $empl) {
+                if ($empl['FirstLogin'] == 1) {
+                  header('Location: homePage.php');
+                  exit;
+                } else {
+                  if ($empl['FirstLogin'] == 0) {
+                    header('Location: changePass.php');
+                  }
+                }
+              }
+              //echo "Profile page";
+            } else {
+              $message = "Wrong data inserted";
+              //or the employee is fired
+              echo $message;
+            }
+          }
         }
-        else
-        {
-            $message = "Wrong data inserted";
-            //or the employee is fired
-            echo $message;
-        }
-    }
-}
-?>
-          <li><input type="submit" name="submit" value="Submit"></li>
-        </ul>
-      </form>
+        ?>
+        <li><input type="submit" name="submit" value="Submit"></li>
+      </ul>
+    </form>
   </div>
 </body>
+
 </html>
